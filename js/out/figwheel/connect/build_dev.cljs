47535f79e642d1e25aeaf82ccdf7877fe036d8f8
julec <julic08@ya.ru>
2017-12-23 15:49:32 +0200

    Update bundle

--- js/out/figwheel/connect/build_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-dev (:require [figwheel.client] [figwheel.client.utils] [app.core]))
-(figwheel.client/start {:build-id "dev", :websocket-url "ws://localhost:3000/figwheel-ws"})
-
--- js/out/figwheel/connect/build_dev.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.9.521 {}
-goog.provide('figwheel.connect.build_dev');
-goog.require('cljs.core');
-goog.require('figwheel.client');
-goog.require('figwheel.client.utils');
-goog.require('app.core');
-figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"build-id","build-id",1642831089),"dev",new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3000/figwheel-ws"], null));
-
-//# sourceMappingURL=build_dev.js.map?rel=1513969496682
